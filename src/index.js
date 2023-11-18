67f36306084aec7f4b191b46958a7f9125cbd94f
deTorresDamian <139564966+deTorresDamian@users.noreply.github.com>
2023-11-18 10:51:16 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const app=require('./config/server');
-require ('./app/routes/clientes')(app);
-
-//iniciar servidor
-
-app.listen(app.get('port'),()=> {
-    console.log('Activo en puerto ', app.get('port'));
-})
